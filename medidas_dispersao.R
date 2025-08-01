3c362b4f6d05ceef5af41894e8900327137292ce
Débora <deboraalves007@hotmail.com>
2025-08-01 16:09:15 -0300

    add comments

--- medidas_dispersao.R
+++ medidas_dispersao.R
@@ -1,30 +1,25 @@
+#Importando o pacote de dados (MASS )e o conjunto de dados (survey)
 library(MASS)
 data(survey)
 
 
-
+#Variância
 variancia_altura <- var(survey$Height, na.rm = TRUE)
-
-desvio_padrao_altura <- sd(survey$Height, na.rm = TRUE)
-
-cat("Altura:\nVariância =", variancia_altura, "\nDesvio Padrão =", desvio_padrao_altura, "\n")
-
-
-
 variancia_pulso <- var(survey$Pulse, na.rm = TRUE)
-
-desvio_padrao_pulso <- sd(survey$Pulse, na.rm = TRUE)
-
-cat("Pulso:\nVariância =", variancia_pulso, "\nDesvio Padrão =", desvio_padrao_pulso, "\n")
-
-
-
 variancia_idade <- var(survey$Age, na.rm = TRUE)
 
+
+#Desvio Padrão
+desvio_padrao_altura <- sd(survey$Height, na.rm = TRUE)
+desvio_padrao_pulso <- sd(survey$Pulse, na.rm = TRUE)
 desvio_padrao_idade <- sd(survey$Age, na.rm = TRUE)
 
+
+#Imprimindo os resultados
+cat("Pulso:\nVariância =", variancia_pulso, "\nDesvio Padrão =", desvio_padrao_pulso, "\n")
+cat("Altura:\nVariância =", variancia_altura, "\nDesvio Padrão =", desvio_padrao_altura, "\n")
 cat("Idade (anos):\nVariância =", variancia_idade, "\nDesvio Padrão =", desvio_padrao_idade, "\n")
 
 
-
+#Clculando o desvio padrão das alturas separadamente para cada grupo de sexo
 tapply(survey$Height, survey$Sex, sd, na.rm = TRUE)
