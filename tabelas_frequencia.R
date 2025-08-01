fbca9ebaf6a5d78689f2efecb28cd5c897ef09aa
Débora <deboraalves007@hotmail.com>
2025-08-01 16:19:21 -0300

    add comments

--- tabelas_frequencia.R
+++ tabelas_frequencia.R
@@ -1,42 +1,43 @@
+#Importando o pacote de dados (MASS )e o conjunto de dados (survey)
 library(MASS)
 data(survey)
 
 
-
+#Tabela de frequência
 tabela_freq_fumo <- table(survey$Smoke)
-tabela_freq_fumo_ordenada <- sort(table(survey$Smoke), decreasing = TRUE)
+tabela_freq_fumo_ordenada <- sort(table(survey$Smoke), decreasing = TRUE) #ordenada
 tabela_freq_exer <- table(survey$Exer)
 tabela_freq_sexo <- table(survey$Sex)
+tabela_freq_idade <- table(survey$AgeGroup)
 
+
+#Tabela de frequência relativa
 tabela_freq_rel_fumo <- prop.table(tabela_freq_fumo) * 100
 tabela_freq_rel_exer <- prop.table(tabela_freq_exer) * 100
 
 
+#Tabela de contingência
+table(survey$Sex, survey$Smoke)
+
+
+#Criando uma nova coluna (AgeGroup) no dataframe survey convertendo as idades em grupos
+survey$AgeGroup <- cut(survey$Age,
+                       breaks = c(10, 20, 30, 40, 80),
+                       labels = c("10-19", "20-29", "30-39", "40+"))
+
+
+#Imprimindo os resultados
 cat("Frequência Fumantes:\n")
 print(tabela_freq_fumo)
-
 print(tabela_freq_fumo_ordenada)
-
 print(round(tabela_freq_rel_fumo, 2))
-
 
 cat("Frequência Exercicios:\n")
 print(tabela_freq_exer)
-
 print(round(tabela_freq_rel_exer, 2))
-
 
 cat("Frequência Gênero:\n")
 print(tabela_freq_exer)
 
-
-
-survey$AgeGroup <- cut(survey$Age, breaks = c(10, 20, 30, 40, 80), labels = c("10-19", "20-29", "30-39", "40+"))
-tabela_freq_idade <- table(survey$AgeGroup)
-
-
 cat("Frequência Idade:\n")
 print(tabela_freq_idade)
-
-
-table(survey$Sex, survey$Smoke)
